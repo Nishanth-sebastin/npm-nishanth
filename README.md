86902cb89a43ac847a840cf21722f61cf9b1f006
Nishanth-sebastin <Nishanthsebastinj24cseb@Gmail.com>
2022-08-20 19:19:41 +0530

    'npm-nishi'

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# npm-nishanth
+# How to use
+
+```bash
+npm i nishanth
+```
+
+## Who am I ?
+
+Use the method to know who I am .
+
+```bash
+const balaji=require('nishanth')
+console.log(nishanth.nishanth());
+```
---
+++ index.js
@@ -0,0 +1,16 @@
+exports.nishanth = () => {
+    return {
+        pronouns: "He" | "Him",
+        askMeAbout: ["app dev", "web dev", "tech"],
+        technologies: {
+            backEnd: ["nodejs", "express"],
+            fronEnd: ["react"],
+            mobileApp: ["java", "react-native"],
+            database: ["mongo", "mySql"],
+            serverless: ["mongo-realm", "aws-lambda"],
+            devOps: ["AWS", "Azure"],
+            misc: ["Firebase"],
+        },
+    }
+
+}
